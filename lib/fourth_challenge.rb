4cf18a49677ef1f2b07aca56f2cba013a00d7bee
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-28 17:58:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,11 +6,15 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         :Benvolio => {
-          :name => "Benvolio"
-          :age => "17"
+          :name => "Benvolio",
+          :age => "17",
           :attitude => "worried"
         }
-        :Mercutio => {}
+        :Mercutio => {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
       ]
    }, 
    :capulet => {
